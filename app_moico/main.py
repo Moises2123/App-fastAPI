c5bcdccac60ac4b113830c3543995e0da5fc8f44
Moises2123 <moisesvasquezrodriguez274@gmail.com>
2025-04-11 17:32:51 -0500

    Codigos arreglados

--- app_moico/main.py
+++ app_moico/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app_moico.routes import router
+import os
 
 app = FastAPI(title="FastAPI CRUD Example")
 
 # Incluir las rutas definidas en `routes.py`
 app.include_router(router, prefix="/users", tags=["Users"])
+
+# Configurar la ruta para servir archivos estáticos
+current_directory = os.path.dirname(os.path.abspath(__file__))  # Obtener la ruta del archivo actual
+static_directory = os.path.join(current_directory, "../static")  # Ir un nivel arriba y luego a la carpeta `static`
+app.mount("/static", StaticFiles(directory=static_directory), name="static")
 
 @app.get("/")
 async def root():
